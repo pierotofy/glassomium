1b63b3aa9e708e2021229709a0a2cde5764c8817
Piero Toffanin <pierotofy@gmail.com>
2012-07-31 18:30:20 -0500

    Added license information to UI Server files, updated license on glassomium client files, added gitignore files for build directory and packaging

--- src/AppConfiguration.cpp
+++ src/AppConfiguration.cpp
@@ -2,8 +2,6 @@
    Glassomium - web-based TUIO-enabled window manager
    http://www.glassomium.org
 
-   Copyright 2012 Piero Toffanin
-
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
