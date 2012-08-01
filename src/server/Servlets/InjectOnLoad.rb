dd87df84f7a15e38e4ead68abafbb9559f8164a4
Piero Toffanin <pierotofy@gmail.com>
2012-07-31 19:05:14 -0500

    Added glassomium authors in the license

--- src/server/Servlets/InjectOnLoad.rb
+++ src/server/Servlets/InjectOnLoad.rb
@@ -2,6 +2,8 @@
    Glassomium - web-based TUIO-enabled window manager
    http://www.glassomium.org
 
+   Copyright 2012 The Glassomium Authors
+
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
