4b82a544447b14f22dcf5b55bcf8c36fcb59d538
Piero Toffanin <pierotofy@gmail.com>
2012-09-06 16:46:12 -0500

    Increased size of OSX menu, created screensaver for OSX theme, fixed version numbers

--- src/server/JS/injectOnLoad.js
+++ src/server/JS/injectOnLoad.js
@@ -6,7 +6,7 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
 var $jsafe = jQuery.noConflict();
 
 /**
-    @namespace Glassomium JS Public API 0.1
+    @namespace Glassomium JS Public API 0.2
 */
 var GLA = GLA || {};
 GLA.keyboardVisible = false;
